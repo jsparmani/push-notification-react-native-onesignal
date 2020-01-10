f19dda2829571f6da2237895051a1ea76d9cfead
Jay Parmani <jsparmani@gmail.com>
2020-01-11 00:23:28 +0530

    Push Notification basic working setup

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View } from "react-native"
+import OneSignal from 'react-native-onesignal';
+
+export default class App extends Component {
+
+  constructor(properties) {
+    super(properties);
+    OneSignal.init("82e9d42d-1281-42e2-95e8-a1fad8067f12");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  render() {
+    return (
+      <View />
+    )
+  }
+}
